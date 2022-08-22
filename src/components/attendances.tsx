001e40fd32663186ff1af9dfa458da8a09fbb779
jungseKim <bible6146@naver.com>
2022-08-22 11:13:16 +0900

    basic

--- src/components/attendances.tsx
+++ src/components/attendances.tsx
@@ -1,26 +1,48 @@
-import { useMemo } from 'react';
-import { Link } from 'react-router-dom';
-import { Iattendance } from '../types/Iattendance';
+import { useMemo } from "react";
+import { Link } from "react-router-dom";
+import { Iattendance } from "../types/Iattendance";
 
 export const timeFomat = function (date: string | Date) {
   const time = new Date(date);
   return `${time.getFullYear()}.${time.getMonth()}.${time.getDate()}`;
 };
 
-const Atttendance = ({ attendace, deleteAtt }: { attendace: Iattendance; deleteAtt: (id: number) => void }) => {
+const Atttendance = ({
+  attendace,
+  deleteAtt,
+}: {
+  attendace: Iattendance;
+  deleteAtt: (id: number) => void;
+}) => {
   const created_at_format = useMemo(() => {
     const time = new Date(attendace.created_at);
     return `${time.getFullYear()}.${time.getMonth()}.${time.getDay()}`;
   }, [attendace]);
 
   return (
-    <div style={{ background: '#F0C292' }} className="flex justify-between   w-full  rounded-xl p-2 pt-4 pb-4">
-      <Link to={'Attendances/' + attendace.id} className="flex justify-between w-5/6">
-        <h1 className="p-3 font-semibold text-base lg:text-xl  text-center">{attendace.title}</h1>
-        <h1 className="p-3 font-semibold text-base lg:text-xl text-center">{attendace.start}</h1>
-        <h1 className="p-3 font-semibold text-base lg:text-xl text-center">{attendace.end}</h1>
-        <h1 className="p-3 font-semibold text-base lg:text-xl text-center">{attendace.rows}</h1>
-        <h1 className="p-3 font-semibold text-base lg:text-xl text-center">{created_at_format}</h1>
+    <div
+      style={{ background: "#F0C292" }}
+      className="flex justify-between   w-full  rounded-xl p-2 pt-4 pb-4"
+    >
+      <Link
+        to={"/Attendances/" + attendace.id}
+        className="flex justify-between w-5/6"
+      >
+        <h1 className="p-3 font-semibold text-base lg:text-xl  text-center">
+          {attendace.title}
+        </h1>
+        <h1 className="p-3 font-semibold text-base lg:text-xl text-center">
+          {attendace.start}
+        </h1>
+        <h1 className="p-3 font-semibold text-base lg:text-xl text-center">
+          {attendace.end}
+        </h1>
+        <h1 className="p-3 font-semibold text-base lg:text-xl text-center">
+          {attendace.rows}
+        </h1>
+        <h1 className="p-3 font-semibold text-base lg:text-xl text-center">
+          {created_at_format}
+        </h1>
       </Link>
       <button
         onClick={() => deleteAtt(attendace.id)}
--- src/pages/AttendaceShow.tsx
+++ src/pages/AttendaceShow.tsx
@@ -1,10 +1,10 @@
-import { useEffect, useMemo, useState } from 'react';
-import { useTable } from 'react-table';
-import { deleteAttendace, getList, showPost } from '../api';
-import { Iattendance } from '../types/Iattendance';
-import { useParams } from 'react-router-dom';
-import { fileFomat } from './main';
-import { IfileFomat, ItableArray } from '../types/IfileFormat';
+import { useEffect, useMemo, useState } from "react";
+import { useSortBy, useTable } from "react-table";
+import { deleteAttendace, getList, showPost } from "../api";
+import { Iattendance } from "../types/Iattendance";
+import { useParams } from "react-router-dom";
+import { fileFomat } from "./main";
+import { IfileFomat, ItableArray } from "../types/IfileFormat";
 const AttendanceShow = () => {
   const [attendance, setAttendance] = useState<IfileFomat[] | []>([]);
   const [temp, setTemp] = useState<ItableArray[] | []>([]);
@@ -57,18 +57,22 @@ const AttendanceShow = () => {
   // ];
   const columns = [
     {
-      Header: 'name',
-      accessor: '이름',
+      Header: "name",
+      accessor: "이름",
     },
     {
-      Header: 'date',
-      accessor: '날짜',
+      Header: "date",
+      accessor: "날짜",
     },
   ];
   const data = [
     {
-      결과: 'ㅇㄹㅇㄹ',
-      날짜: '34343ㅇㄹㅇ',
+      이름: "ㅇㄹㅇㄹ",
+      날짜: "34343ㅇㄹㅇ",
+    },
+    {
+      이름: "ㅇㄹㅇㄹ",
+      날짜: "34343ㅇㄹㅇ",
     },
   ];
   const {
@@ -86,50 +90,90 @@ const AttendanceShow = () => {
     setPageSize,
     state,
     prepareRow,
-  } = useTable({
-    // @ts-ignore
-    columns: columns,
-    data: data,
-  });
+    rows,
+  } = useTable(
+    {
+      // @ts-ignore
+      columns: columns,
+      data: data,
+    },
+    useSortBy
+  );
   useEffect(() => {
     if (attendancesId === undefined) return;
-    console.log('ㅇㄹㅇㄹㅇ');
+    console.log("ㅇㄹㅇㄹㅇ");
     // 여기서 에러 원인은 모름
-    showPost(parseInt(attendancesId)).then((data) => {
-      // console.log(data);
-      setAttendance(data.IfileFomat);
-    });
-  }, [attendancesId]);
+    // showPost(parseInt(attendancesId)).then((data) => {
+    //   // console.log(data);
+    //   setAttendance(data.IfileFomat);
+    // });
+  }, []);
 
+  useEffect(() => {
+    console.log(headerGroups, getTableBodyProps());
+  }, [headerGroups, getTableBodyProps]);
   return (
     <div className="w-full flex flex-col w-full gap-10 items-center p-10">
-      <h1 className=" font-bold text-5xl lg:text-7xl font-mono">Attendance book</h1>
-      {/* <div className="table">
-        <table {...getTableProps()}>
-          <thead>
-            {headerGroups.map((headerGroup) => (
-              <tr {...headerGroup.getHeaderGroupProps()}>
-                {headerGroup.headers.map((column) => (
-                  <th {...column.getHeaderProps()}>{column.render('Header')}</th>
-                ))}
-              </tr>
-            ))}
-          </thead>
-
-          <tbody {...getTableBodyProps()}>
-            {page.map((row: any) => {
-              prepareRow(row);
-              return (
-                <tr {...row.getRowProps()}>
-                  {row.cells.map((cell: any) => {
-                    return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>;
+      <h1 className=" font-bold text-5xl lg:text-7xl font-mono">
+        Attendance book
+      </h1>
+      <div className="mt-2 flex flex-col">
+        <div className="-my-2 overflow-x-auto -mx-4 sm:-mx-6 lg:-mx-8">
+          <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+            <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+              <table
+                {...getTableProps()}
+                className="min-w-full divide-y divide-gray-200"
+              >
+                <thead className="bg-gray-50">
+                  {headerGroups.map((headerGroup) => (
+                    <tr {...headerGroup.getHeaderGroupProps()}>
+                      {headerGroup.headers.map((column) => (
+                        // Add the sorting props to control sorting. For this example
+                        // we can add them into the header props
+                        <th
+                          scope="col"
+                          className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                          {...column.getHeaderProps(
+                            column.getSortByToggleProps()
+                          )}
+                        >
+                          {column.render("Header")}
+                          {/* Add a sort direction indicator */}
+                          <span>
+                            {column.isSorted
+                              ? column.isSortedDesc
+                                ? " ▼"
+                                : " ▲"
+                              : ""}
+                          </span>
+                        </th>
+                      ))}
+                    </tr>
+                  ))}
+                </thead>
+                <tbody
+                  {...getTableBodyProps()}
+                  className="bg-white divide-y divide-gray-200"
+                >
+                  {rows.map((row) => {
+                    prepareRow(row);
+                    return (
+                      <tr {...row.getRowProps()}>
+                        {row.cells.map((cell) => (
+                          <td {...cell.getCellProps()}>
+                            {cell.render("Cell")}
+                          </td>
+                        ))}
+                      </tr>
+                    );
                   })}
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div> */}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
